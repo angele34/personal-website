e1ea6e9bf456c5bc4782683db16d07747bf8f426
angele34 <90606365+angele34@users.noreply.github.com>
2024-08-27 14:11:36 +0800

    fix navbar links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,10 +30,10 @@ export default function Navbar() {
       </div>
 
       <div className="sm:hidden xs:hidden xxs:hidden flex items-center space-x-4 pr-20">
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className="cursor-pointer text-white hover:text-customGreenLight transition delay-100">
+        <a href="https://github.com/angele34" target="_blank" rel="noopener noreferrer" className="cursor-pointer text-white hover:text-customGreenLight transition delay-100">
           <FaGithub size={25} />
         </a>
-        <a href="https://www.linkedin.com/in/" target="_blank" rel="noopener noreferrer" className="cursor-pointer text-white hover:text-customGreenLight transition delay-100">
+        <a href="https://www.linkedin.com/in/angel-arwen-e-3a1b68246/" target="_blank" rel="noopener noreferrer" className="cursor-pointer text-white hover:text-customGreenLight transition delay-100">
           <FaLinkedin size={25} />
         </a>
       </div>
