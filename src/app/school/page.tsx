1e8d926476db05341feb00ca7248150f91c084fc
angele34 <90606365+angele34@users.noreply.github.com>
2024-09-25 19:31:34 +0800

    add School page content

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -19,8 +19,43 @@ export default function School() {
           className="relative z-10 flex-grow"
         >
           <Navigation />
+          <div className="relative whitespace-normal pt-20 pb-8 pl-40 xxs:px-2 xs:px-8 sm:px-4 md:py-16 md:px-8 lg:px-8 xl:mr-52">
 
-          <div className="absolute bottom-0 w-full z-10">
+            {/* Title */}
+            <div className='pt-16 md:pt-32'>
+              <h1 className='text-customGreenLight pl-4 break-words font-helvetica font-semibold tracking-widest text-opacity-90 xxs:text-h1-xxs xs:text-h1-xs sm:text-h1-sm md:text-h1-md lg:text-h1-lg xl:text-h1-xl 2xl:text-h1-2xl mb-8'>
+                School.
+              </h1>
+            </div>
+
+            {/* Description */}
+            <div className="pl-4">
+              <p className=' text-content-color font-instrument_sans font-normal xxs:text-p-xxs xs:text-p-xs sm:text-p-sm md:text-p-md lg:text-p-lg xl:text-p-lg 2xl:text-p-2xl mb-4 md:mb-6 tracking-wider'>
+                I&apos;m currently an undergrad student at DLSU taking CS with a major in Computer Systems Engineering. You can read about the relevant courses I&apos;m taking and what things I&apos;m currently involved in here!
+              </p>
+            </div>
+
+            <div className='pt-8'>
+              <h1 className="text-white px-4 sm:pl-8 xxs:pl-12 font-normal tracking-widest break-words" style={{ textDecoration: 'underline', textDecorationColor: '#A3C269', fontSize: '2.4rem' }} >
+              2024
+              </h1>
+            </div>
+        </div>
+
+        {/* Cards */}
+        
+        
+        {/* style={{ background: 'linear-gradient(to , #eaf6cf, #A3C269)' }} */}
+
+        <div className="xl:items-center xl:justify-center pb-28 text-center px-4">
+          <div className="grid gap-2 2xl:pl-40 2xl:pr-40 xl:pl-40 xl:pr-40 pl-8 pr-8 2xl:grid-cols-3 xl:grid-cols-3">
+            <div className="bg-customGreenLight p-3">CCICOMP <br /> Introduction to Computing</div>
+            <div className="bg-custom2 p-3">CCPROG1 <br /> Logic Formulation and Introductory Programming</div>
+            <div className="bg-customGreenLight p-3">MTH101A <br /> Foundation Course in Mathematics</div>
+        </div>
+       </div>
+
+          <div className="bottom-0 w-full z-10">
             <Footer />
           </div>
         </motion.div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         'p-xxs': ['0.8rem', '1.2rem'],  
         'p-xs': ['0.9rem', '1.4rem'],        
         'p-md': ['1.1rem', '1.4rem'],        
-        'p-lg': ['1.11rem', '1.4rem'],       
+        'p-lg': ['1.11rem', '1.7rem'],       
         'p-xl': ['1.15rem', '1.7rem'],          
         'p-2xl': ['1.4rem', '2.2rem'],       
       
@@ -24,7 +24,7 @@ module.exports = {
         'h1-xs': ['2.4rem', '3rem'],         
         'h1-sm': ['3rem', '4rem'],           
         'h1-md': ['3.5rem', '4.5rem'],       
-        'h1-lg': ['4rem', '4.5rem'],           
+        'h1-lg': ['4rem', '4.8rem'],           
         'h1-xl': ['4.2rem', '6rem'],         
         'h1-2xl': ['5rem', '7rem'],   
         
@@ -61,7 +61,11 @@ module.exports = {
         'primary': '#FF6363',
         'customGreen': '#70B835',
         'customGreenLight': '#A3C269',
+        'custom2' : '#eaf6cf',
+        'custom3' : '#f8fff8',
+        'custom4' : '#88a750',
         'content-color': '#aeb0aef1',
+        
       },
 
       fontWeight: {
