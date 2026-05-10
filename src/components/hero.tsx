7e212670f1e71c79c6ee838a2cdb2000a53975fc
angele34 <90606365+angele34@users.noreply.github.com>
2026-05-10 14:47:19 +0800

    Fix responsiveness in Hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import '../styles/globals.css';
 import { TypeAnimation } from 'react-type-animation';
 
 export default function Hero() {
   return (
-    <div className="relative pb-8 mb-10 px-4 sm:px-8 md:px-16 lg:px-32 flex justify-center">
+    <div className="relative pb-8 mb-10 px-40 sm:px-8 md:px-16 lg:px-8 xl:px-4 flex justify-center">
       {/* Gradient Circle on the Left */}
       <div className="absolute custom-top left-[57%] md:left-[45%] sm:left-[28%] xs:left-[25%] transform -translate-y-1/2 w-[30rem] h-[26rem] bg-gradient-radial from-[#91aa5f]/30 to-[#121010]/30 blur-[100px] opacity-80 rounded-full z-0">
       </div>
